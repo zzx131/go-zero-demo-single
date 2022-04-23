9f2626d51fb0bd9b8b9e0aefa8b3f805dd157bf4
zzx <zzx_nuc@163.com>
2022-04-23 18:18:45 +0800

    go-zero 获取token里面的信息

--- app/usercenter/cmd/api/internal/handler/user/userInfoHandler.go
+++ app/usercenter/cmd/api/internal/handler/user/userInfoHandler.go
@@ -1,17 +1,27 @@
 package user
 
 import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/zeromicro/go-zero/rest/token"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/logic/user"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/response"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/svc"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/types"
 	"net/http"
+	"time"
 
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 func UserInfoHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// 获取token信息
+		parser := token.NewTokenParser(token.WithResetDuration(time.Second))
+		tok, err := parser.ParseToken(r, svcCtx.Config.JwtAuth.AccessSecret, "")
+		value := tok.Claims.(jwt.MapClaims)["key"]
+		fmt.Println(value)
+
 		var req types.UserInfoReq
 		if err := httpx.Parse(r, &req); err != nil {
 			httpx.Error(w, err)
--- app/usercenter/cmd/api/internal/logic/user/userInfoLogic.go
+++ app/usercenter/cmd/api/internal/logic/user/userInfoLogic.go
@@ -3,7 +3,6 @@ package user
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/zeromicro/go-zero/core/logx"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/svc"
 	"go-zero-demo-single/app/usercenter/cmd/api/internal/types"
@@ -25,13 +24,12 @@ func NewUserInfoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserInfo
 }
 
 func (l *UserInfoLogic) UserInfo(req *types.UserInfoReq) (resp *types.UserInfoResp, err error) {
-	fmt.Println("userinfo.....")
 	user, err := l.svcCtx.UserModel.FindOne(l.ctx, req.UserId)
 	if err != nil && err != model.ErrNotFound {
 		return nil, errors.New("查询数据失效！")
 	}
 	if user == nil {
-		return nil, errors.New("用户不存在222444！")
+		return nil, errors.New("用户不存在！")
 	}
 	return &types.UserInfoResp{
 		UserId:   user.Id,
